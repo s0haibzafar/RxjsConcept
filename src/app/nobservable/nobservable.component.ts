14ceed85b4eff698fd742735adc45aad89248c06
S0haibZafar <sohaibzafar71@gmail.com>
2024-01-10 22:01:37 +0500

    Create observable component to list of obs

---
+++ src/app/nobservable/nobservable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nobservable',
+  templateUrl: './nobservable.component.html',
+  styleUrls: ['./nobservable.component.css']
+})
+export class NobservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
